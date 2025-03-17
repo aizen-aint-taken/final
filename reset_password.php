4608dade538027de4f88d4bf0068dc3269033cd0
JB-SINDAY <votivemitten230sinday@gmail.com>
2025-03-17 14:52:32 +0800

    hashed pass

--- reset_password.php
+++ reset_password.php
@@ -4,15 +4,12 @@ if (isset($_POST['reset_password'])) {
     $new_password = $_POST['new_password'];
     $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
 
-    // Update in users table
     $stmt = $conn->prepare("UPDATE users SET password = ? WHERE email = ?");
     $stmt->bind_param("ss", $hashed_password, $email);
     $stmt->execute();
 
-    // Update in admin table
+
     $stmt = $conn->prepare("UPDATE admin SET password = ? WHERE email = ?");
     $stmt->bind_param("ss", $hashed_password, $email);
     $stmt->execute();
-
-    // Add success message
 }
